0260beb8673688c7232212562eae2b9cf7a43c06
shriram-mnit <43758704+shriram-mnit@users.noreply.github.com>
2020-04-02 14:34:03 +0530

    simpleserver

---
+++ server.js
@@ -0,0 +1,53 @@
+var http=require('http');
+var url =require('url');
+var path=require('path');
+var fs=require('fs');
+
+
+var mimeTypes ={
+	"html": "text/html",
+    "jpeg": "image/jpeg",
+    "jpg": "image/jpeg",
+    "png": "image/png",
+    "js": "text/javascript",
+    "css": "text/css"};
+
+// sever function
+http.createServer(function(req,res){
+	var uri = url.parse(req.url).pathname;
+  var filename = path.join(process.cwd(), unescape(uri));
+  var stats;
+
+  try {
+    stats = fs.lstatSync(filename); // throws if path doesn't exist
+  } 
+  catch (e) {
+    res.writeHead(404, {'Content-Type': 'text/plain'});
+    res.write('404 Not Found\n');
+    res.end();
+    return;
+  }
+
+
+  if (stats.isFile()) {
+    // path exists, is a file
+    var mimeType = mimeTypes[path.extname(filename).split(".").reverse()[0]];
+    res.writeHead(200, {'Content-Type': mimeType} );
+
+    var fileStream = fs.createReadStream(filename);
+    fileStream.pipe(res);
+  } else if (stats.isDirectory()) {
+    // path exists, is a directory
+    res.writeHead(200, {'Content-Type': 'text/plain'});
+    res.write('Index of '+uri+'\n');
+    res.end();
+	return;
+  } else {
+
+    res.writeHead(500, {'Content-Type': 'text/plain'});
+    res.write('500 Internal server error\n');
+    res.end();
+	return;
+  }
+
+}).listen(3000);
